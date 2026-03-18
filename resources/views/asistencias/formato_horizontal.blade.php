9079450bc32968930d09ff60fcb4805a34840762
Levid <beniezlev@gmail.com>
2026-03-18 10:39:57 -0600

    Restaurada tabla en encabezado para asegurar visibilidad de pleca ( floats desaparecían en PDF)

--- resources/views/asistencias/formato_horizontal.blade.php
+++ resources/views/asistencias/formato_horizontal.blade.php
@@ -119,16 +119,18 @@
     </style>
 </head>
 <body>
-    <div style="width: 100%; overflow: hidden; margin-bottom: 5px;">
-        <div style="float: left; width: 70%;">
-            <img src="{{ public_path('img/pleca.png') }}" style="height: 40px; width: auto;" alt="Institucional">
-        </div>
-        <div style="float: right; width: 28%; text-align: right; vertical-align: middle; font-size: 8.5px; font-weight: bold; text-transform: uppercase; padding-top: 12px;">
-            {{ $grupo->plantel->nombre }}
-        </div>
-    </div>
+    <table style="width: 100%; border-collapse: collapse; margin-bottom: 5px;">
+        <tr>
+            <td style="width: 70%; text-align: left; vertical-align: top;">
+                <img src="{{ public_path('img/pleca.png') }}" style="height: 45px; width: auto;" alt="Institucional">
+            </td>
+            <td style="width: 30%; text-align: right; vertical-align: middle; font-size: 8px; font-weight: bold; text-transform: uppercase;">
+                {{ $grupo->plantel->nombre }}
+            </td>
+        </tr>
+    </table>
 
-    <div style="clear: both; text-align: center; border-top: 0.5px solid #000; padding-top: 2px;">
+    <div style="text-align: center; border-top: 0.8px solid #000; margin-top: 2px; padding-top: 2px;">
         <div style="font-weight: bold; font-size: 8px; text-transform: uppercase;">
             LISTA DE ASISTENCIA
         </div>
